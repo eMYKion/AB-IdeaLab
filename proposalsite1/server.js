3debb37ad63fe2344a39676f24f7ec5764a2af78
eMYKion <eMYKion@users.noreply.github.com>
2014-11-14 16:26:28 -0500

    many major miscellaneous updates

--- proposalsite1/server.js
+++ proposalsite1/server.js
@@ -1,13 +1,15 @@
-var fs = require('fs');
 
-str=prompt("Type in your message:");
-str=str.toString();
-/*
-fs.writeFile("example_log.txt", str, function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
+function logincheck(){
+    usn = document.getElementById("usn").value;
+    psw = document.getElementById("psw").value;
+    
+    //console.log("loginDataAs: "usn+","+psw);
+    
+    if((usn=="IdeaLabUser") && (psw=="IdeaLabPsw")){
+        
+        console.log("Login Succesful, "+usn);
+        document.getElementById("signin").innerHTML="\
+        <p>Welcome User:</p>"+"<a href=\"https://www.facebook.com/groups/806895806041239/\">Dev Facebook Group</a>"
+    
     }
-});
-*/
+}
